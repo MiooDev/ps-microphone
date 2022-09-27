0ea7eeae0cbd77d2555798c4c87d0a7eee68cd2a
Mio <101432216+MiooDev@users.noreply.github.com>
2022-09-26 22:05:27 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,11 +1,7 @@
---[[ local QBCore = exports['qb-core']:GetCoreObject()
-QBCore.Functions.CreateUseableItem("rightturnsign", function(source, item)
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateUseableItem("megaphone", function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    TriggerClientEvent('megaphone:Toggle', src)
-end)
-]]
-
-exports['lj-inventory']:CreateUsableItem("megaphone", function(source,item)
-    TriggerClientEvent("megaphone:Toggle",source)
+    TriggerClientEvent('megaphone:Toggle', source)
 end)
